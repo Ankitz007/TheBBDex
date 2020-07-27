a6578fd295458c70540a92f81715c68f1929e630
Ankitz007 <ankitkumarsingh11061999@gmail.com>
2020-07-27 23:34:18 +0530

    Updates

---
+++ precache-manifest.b17dccc940bff138268045828bf1320f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f0e01bbe308150b691dac1f1bde31af",
+    "url": "/thebbdex/index.html"
+  },
+  {
+    "revision": "a7af6fe5628a20439fcd",
+    "url": "/thebbdex/static/css/main.59a8b8ae.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/thebbdex/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/thebbdex/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7af6fe5628a20439fcd",
+    "url": "/thebbdex/static/js/main.dfef3c11.chunk.js"
+  },
+  {
+    "revision": "da9f501ac0edf2878d1a",
+    "url": "/thebbdex/static/js/runtime-main.ec5bb01d.js"
+  }
+]);
