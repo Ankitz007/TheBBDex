a57e177b43c0244d4c2cd254869413bd4f22686e
Ankitz007 <ankitkumarsingh11061999@gmail.com>
2020-07-28 00:07:46 +0530

    Updates

---
+++ precache-manifest.0ff0657210ddcee5ec1d649343eacb5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a4b4aef735c8387142dc12136f0da8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0c110ce9cbee16a19000",
+    "url": "./static/css/main.59a8b8ae.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "./static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c110ce9cbee16a19000",
+    "url": "./static/js/main.552e39c4.chunk.js"
+  },
+  {
+    "revision": "7304c6cd1204852fb3fb",
+    "url": "./static/js/runtime-main.1451e3c5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/thebbdex/precache-manifest.b17dccc940bff138268045828bf1320f.js"
+  "./precache-manifest.0ff0657210ddcee5ec1d649343eacb5e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/thebbdex/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
